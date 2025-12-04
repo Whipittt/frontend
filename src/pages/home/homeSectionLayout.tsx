acc48d34a2f5801dfbe2abcbb12f7f4215d000f3
Chinweike Igwenagu <151936141+Nweike-Igwenagu@users.noreply.github.com>
2025-12-04 09:24:03 +0100

    Adjust header font size in homeSectionLayout

--- src/pages/home/homeSectionLayout.tsx
+++ src/pages/home/homeSectionLayout.tsx
@@ -18,7 +18,7 @@ export default function HomeSectionLayout({
   return (
     <section className="flex flex-col gap-6 mt-4">
       <div className="flex flex-col gap-1">
-        <h2 className="px-4 md:px-0 font-semibold text-md md:text-lg capitalize">
+        <h2 className="px-4 md:px-0 font-semibold text-lg md:text-base capitalize">
           {header}
         </h2>
         {description && (
