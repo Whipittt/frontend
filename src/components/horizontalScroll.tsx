1f23de0e0fbf682270b8dbd27b68223e9fef89e8
Chinweike Igwenagu <151936141+Nweike-Igwenagu@users.noreply.github.com>
2025-12-04 08:24:43 +0100

    Refactor grid layout in horizontalScroll component

--- src/components/horizontalScroll.tsx
+++ src/components/horizontalScroll.tsx
@@ -13,7 +13,7 @@ export default function HorizontalScroll({
   return (
     <div
       className={cn(
-        "w-full grid md:grid-cols-6 grid-cols-2 gap-8 hide-scrollbar md:px-0 px-4",
+        "w-full grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 hide-scrollbar md:px-0 px-4",
         className
       )}
     >
